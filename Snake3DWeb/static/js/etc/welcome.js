af0e35902768dd6cdfe4ec64a096c56b352ba810
MenesesGHZ <gerardo.meneses@cetys.edu.mx>
2020-10-14 19:18:20 +0000

    added welcome.js

--- Snake3DWeb/static/js/etc/welcome.js
+++ Snake3DWeb/static/js/etc/welcome.js
@@ -1,36 +1,44 @@
 window.addEventListener('load',()=>{
     let text_el = document.getElementById("welcome-content"),
-        welcome_text = ` 
+        welcome_text = `
         <div class="row my-3 justify-content-center">
             <div class="col-10">
                 <p class="font-times text-center" >
-                    We are two students from CETYS University that are currently studying 
+                    We are two students from CETYS University that are currently studying
                     computational science in fourth semester. This platform was a final project
-                    of computer graphics asignature, but we wanted
+                    of computer graphics course, but we wanted
                     to develop more than an oriented graphic project.
                 </p>
             </div>
         </div>
      `;
-    welcome_text += ` 
+  welcome_text += `
     <div class="row mt-3 mb-2 justify-content-center">
         <p class="font-times"> Developer Team: </p>
     </div>
-    <div class="row justify-content-around mx-4 ">
-        <p class="font-times">
-        Gerardo Hern&#225;ndez Meneses 
-        </p> 
+    <div class="row justify-content-center text-center  mb-3 mb-md-1 mb-lg-1">
+    <div class="col-lg-6 col-md-12 col-sm-12">
+    <p class="font-times">
+    Gerardo Hern&#225;ndez Meneses
+    </p>
+        </div>
+        <div class="col-lg-6 col-md-12 col-sm-12">
         <p class="font-times" style="color:#961bff">
-            AI and Game Programmer
-        </p> 
+        AI and Game Programmer
+    </p>
+        </div>
     </div>
-    <div class="row justify-content-around mx-4">
-        <p class="font-times">
-        Pablo Diaz Ochoa
-        </p> 
+    <div class="row justify-content-center text-center ">
+    <div class="col-lg-6 col-md-12 col-sm-12">
+    <p class="font-times">
+    Pablo Diaz Ochoa
+    </p>
+        </div>
+        <div class="col-lg-6 col-md-12 col-sm-12">
         <p class="font-times" style="color:#961bff">
-           Game Programmer
-        </p> 
+        Game Programmer
+     </p>
+        </div>
     </div>`;
     welcome_text += `
     <div class="row my-4 justify-content-center align-items-center">
